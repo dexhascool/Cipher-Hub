4a2c1778dc19b76d92e482ea8cc221c827c4ed35
thelonious-jaha <daxtonf11@gmail.com>
2025-01-09 21:46:46 -0700

    Create ROT47.lua
    Added ROT47 Cipher

---
+++ ciphers/ROT47.lua
@@ -0,0 +1,57 @@
+if not _G.CipherUtils then
+    warn("CipherUtils not found! Load the loader script first.")
+    return
+end
+
+local CipherUtils = _G.CipherUtils
+local log = CipherUtils.log
+local chatMessage = CipherUtils.chatMessage
+
+local function rot47Cipher(input)
+    local result = {}
+    for i = 1, #input do
+        local char = input:sub(i, i)
+        local byte = char:byte()
+
+        if byte >= 33 and byte <= 126 then
+            byte = 33 + ((byte - 33 + 47) % 94)
+        end
+
+        table.insert(result, string.char(byte))
+    end
+    return table.concat(result)
+end
+
+local baseUi = loadstring(game:HttpGet("https://raw.githubusercontent.com/thelonious-jaha/Cipher-Hub/refs/heads/main/extras/UILib.lua"))()
+
+local uiElements = 3
+local frame = baseUi.createUi("ROT47 Cipher", uiElements)
+
+local inputBox = baseUi.addInputBox(frame, "Enter text", UDim2.new(0, 10, 0, 50))
+local encodeButton = baseUi.addEncodeButton(frame, UDim2.new(0, 20, 0, 100))
+local resultLabel = baseUi.addResultLabel(frame, UDim2.new(0, 10, 0, 150))
+
+local lastInputText = ""
+
+encodeButton.MouseButton1Click:Connect(function()
+    local inputText = inputBox.Text
+
+    if inputText == "" then
+        log("Input is blank. No encoding performed.")
+        resultLabel.Text = "Input is blank"
+        return
+    end
+
+    if inputText == lastInputText then
+        log("Input is identical to the last processed text. No encoding performed.")
+        resultLabel.Text = "Input is the same as last time"
+        return
+    end
+
+    lastInputText = inputText
+    local encodedText = rot47Cipher(inputText)
+    resultLabel.Text = "<b>Encoded:</b> " .. encodedText
+
+    log("Encoded: " .. encodedText)
+    chatMessage(encodedText)
+end)
