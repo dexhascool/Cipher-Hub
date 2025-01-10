45a5e3e57261a5feaf5fa9bd27ed9665a339aa07
thelonious-jaha <daxtonf11@gmail.com>
2025-01-09 21:30:26 -0700

    Create Atbash.lua
    Added Atbash Cipher

---
+++ ciphers/Atbash.lua
@@ -0,0 +1,54 @@
+if not _G.CipherUtils then
+    warn("CipherUtils not found! Load the loader script first.")
+    return
+end
+
+local CipherUtils = _G.CipherUtils
+local log = CipherUtils.log
+local chatMessage = CipherUtils.chatMessage
+
+local atbashMap = {}
+for i = 65, 90 do atbashMap[string.char(i)] = string.char(90 - (i - 65)) end -- Uppercase
+for i = 97, 122 do atbashMap[string.char(i)] = string.char(122 - (i - 97)) end -- Lowercase
+
+local function atbashCipher(text)
+    local result = {}
+    for char in text:gmatch(".") do
+        table.insert(result, atbashMap[char] or char)
+    end
+    return table.concat(result)
+end
+
+local baseUi = loadstring(game:HttpGet("https://pastebin.com/raw/XuC5DavN"))()
+
+local uiElements = 3
+local frame = baseUi.createUi("Atbash Cipher", uiElements)
+
+local inputBox = baseUi.addInputBox(frame, "Enter text", UDim2.new(0, 10, 0, 50))
+local encodeButton = baseUi.addEncodeButton(frame, UDim2.new(0, 20, 0, 100))
+local resultLabel = baseUi.addResultLabel(frame, UDim2.new(0, 10, 0, 150))
+
+local lastInputText = ""
+
+encodeButton.MouseButton1Click:Connect(function()
+    local inputText = inputBox.Text
+
+    if inputText == "" then
+        log("Input is blank. No encoding performed.")
+        resultLabel.Text = "Input is blank"
+        return
+    end
+
+    if inputText == lastInputText then
+        log("Input is identical to the last processed text. No encoding performed.")
+        resultLabel.Text = "Input is the same as last time"
+        return
+    end
+
+    lastInputText = inputText
+    local encodedText = atbashCipher(inputText)
+    resultLabel.Text = "<b>Encoded:</b> " .. encodedText
+
+    log("Encoded: " .. encodedText)
+    chatMessage(encodedText)
+end)
