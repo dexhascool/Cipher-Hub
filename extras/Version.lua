7a2319a074f28b27ddfe9588d437c430dabc8519
thelonious-jaha <daxtonf11@gmail.com>
2025-01-13 17:42:56 -0700

    Create Version.lua
    Added Version

---
+++ extras/Version.lua
@@ -0,0 +1,10 @@
+return {
+    version = "1.0.0",
+    releaseDate = "2025-01-13",
+    description = "Added version checking and changelogs.",
+    changelog = {
+        "Added version checking.",
+        "Implemented changelogs.",
+        "Improved error handling in the loader.",
+    }
+}
