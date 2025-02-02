c92855c1fffe3f42899f3d8f87deca4193704be4
thelonious-jaha <daxtonf11@gmail.com>
2025-02-02 09:49:48 -0700

    Update Version.lua

--- extras/Version.lua
+++ extras/Version.lua
@@ -5,7 +5,6 @@ return {
     changelog = {
         "Added Blowfish Cipher.",
         "Added Magma Cipher (may be inaccurate).",
-        "Minor improvements and optimizations in the laoder.",
         "Added 'Chat Encoded Message' toggle to control whether the encoded chat message is automatically chatted or not."
     }
 }
